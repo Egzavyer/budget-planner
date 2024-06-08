0294c11ed45e6a1b1ac362fee4e20bd781613bee
Egzavyer <xavier.lermusieaux@gmail.com>
2024-06-08 12:16:07 -0400

    Chore: add test project

---
+++ budget-planner-test/budget-planner-test/budget-planner-test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace budgetplannertest
+{
+	TEST_CLASS(budgetplannertest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
