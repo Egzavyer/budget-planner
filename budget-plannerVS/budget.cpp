005a0adc68be3aeed826e0880169040a8f8511a4
Egzavyer <xavier.lermusieaux@gmail.com>
2024-06-15 17:52:01 -0400

    Fix: clean up

--- budget-plannerVS/budget.cpp
+++
@@ -1,3 +0,0 @@
-#include "budget.h"
-
-
--- budget-plannerVS/budget.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUDGET_PLANNER_BUDGET_H
-#define BUDGET_PLANNER_BUDGET_H
-
-#include <iostream>
-#include <string>
-#include <pqxx/pqxx>
-
-
-#endif //BUDGET_PLANNER_BUDGET_H
